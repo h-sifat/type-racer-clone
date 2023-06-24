79c3f411d2c646fbff42ec8bc41f8ec9c11c5bdd
Sifat Hossain <80267830+h-sifat@users.noreply.github.com>
2023-06-24 12:22:04 +0600

    feat(App): add TestBoard control button

--- src/App.svelte
+++ src/App.svelte
@@ -1,24 +1,80 @@
+<script context="module" lang="ts">
+  const controlBtnStateToTextMap = Object.freeze({
+    running: "Stop",
+    stopped: "Reset",
+    not_started: "Start",
+  });
+
+  type ButtonState = keyof typeof controlBtnStateToTextMap;
+  const text = `Thing is Butch, right now you got ability. But painful as it may be, ability don't last. Now that's a hard fact of life, but it's a fact of life you're gonna have to get realistic about.`;
+</script>
+
 <script lang="ts">
-  import { onMount } from "svelte";
+  import type { KeypressLog } from "./components/TestBoard/interface";
 
   import TestBoard from "./components/TestBoard/TestBoard.svelte";
-  const text = `Thing is Butch, right now you got ability. But painful as it may be, ability don't last. Now that's a hard fact of life, but it's a fact of life you're gonna have to get realistic about.`;
 
   let testBoard: TestBoard;
+  let btnState: ButtonState = "not_started";
+  let textBoardKey = 0;
+  let startTime: number, endTime: number;
+
+  function handleControl() {
+    switch (btnState) {
+      case "not_started":
+        startTime = Date.now();
+        testBoard.start();
+        btnState = "running";
+        break;
+
+      case "running":
+        endTime = Date.now();
+        testBoard.end({ message: "Manually ended" });
+        btnState = "stopped";
+        break;
+
+      case "stopped":
+        textBoardKey++;
+        btnState = "not_started";
+        break;
+    }
+  }
+
+  $: btnText = controlBtnStateToTextMap[btnState];
+  // $: console.log(btnText);
 
   function onComplete() {
     console.log("test completed");
+    btnState = "stopped";
+    endTime = Date.now();
   }
 
-  onMount(() => {
-    // testBoard.start();
-    //
-    // setTimeout(() => {
-    //   testBoard.end({ message: "Time up" });
-    // }, 10_000);
-  });
+  function onKeypress(log: KeypressLog) {
+    // console.log(log);
+  }
 </script>
 
 <main>
-  <TestBoard bind:this={testBoard} {text} {onComplete} />
+  {#key textBoardKey}
+    <TestBoard bind:this={testBoard} {text} {onComplete} {onKeypress} />
+  {/key}
+
+  <div class="controls">
+    <button class="control-btn" on:click|trusted={handleControl}
+      >{btnText}</button
+    >
+  </div>
 </main>
+
+<style>
+  .controls {
+    display: flex;
+    justify-content: center;
+    margin-top: 1em;
+  }
+
+  .control-btn {
+    padding: 0.5em 1em;
+    cursor: pointer;
+  }
+</style>
